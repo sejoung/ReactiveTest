efa23ae54ecb737a98078c6f87f4d319e6d96814
sejoung kim <sejoung@gmail.com>
2018-05-18 12:03:59 +0900

    스템프결합

---
+++ src/main/java/com/github/sejoung/reactive/test/StampCode.java
@@ -0,0 +1,12 @@
+package com.github.sejoung.reactive.test;
+
+public class StampCode {
+
+    public static void printAge(Data data){
+        System.out.println(data.getAge());
+    }
+
+    public static void printName(Data data){
+        System.out.println(data.getName());
+    }
+}
